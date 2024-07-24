4e96b5dd047532445a2af1e7218a6327a1e25943
Tatiana Grigorieva <tsg1204@gmail.com>
2024-07-24 16:46:52 -0400

    added template section with video player

--- JS/script.js
+++ JS/script.js
@@ -21,8 +21,178 @@ filterLinks.forEach((filterLink) => {
       filterLink.getAttribute('data-type') === project.getAttribute('data-type') || filterLink.getAttribute('data-type') === 'all' ? project.classList.remove('hide') : ''
     })
   })
+})
 
+const videoContainer = document.querySelector('.video-container')
+const mainVideo = document.querySelector('video')
+const playPauseBtn = document.querySelector('.play-pause i')
+const progressBar = document.querySelector('.progress-bar')
+const skipBackward = document.querySelector('.skip-backward i')
+const skipForward = document.querySelector('.skip-forward i')
+const volumeBtn = document.querySelector('.volume i')
+const volumeSlider = document.querySelector('.left input')
+const speedBtn = document.querySelector('.playback-speed span')
+const speedOptions = document.querySelector('.speed-options')
+const speedOptionsItems = document.querySelectorAll('.speed-options div')
+const picInPicBtn = document.querySelector('.pic-in-pic span')
+const fullscreenBtn = document.querySelector('.fullscreen i')
+const videoTimeline = document.querySelector('.video-timeline')
+const currentVideoTime = document.querySelector('.current-time')
+const videoDuration = document.querySelector('.video-duration')
+const playButton = document.querySelector('.play-btn')
+const xButton = document.querySelector('.x-btn i')
+let timer
+
+playButton.addEventListener('click', () => {
+  videoContainer.classList.add('show-video')
+})
+
+xButton.addEventListener('click', () => {
+  videoContainer.classList.remove('show-video')
+  mainVideo.pause()
 })
 
 
+//controls
+const hideControls = () => {
+  if (mainVideo.paused) return
 
+  timer = setTimeout(() => {
+    videoContainer.classList.remove('show-controls')
+  }, 3000)
+}
+
+hideControls()
+
+videoContainer.addEventListener('mousemove', () => {
+  videoContainer.classList.add('show-controls')
+  clearTimeout(timer)
+  hideControls()
+})
+//progress bar
+const formatTime = (time) => {
+  let seconds = Math.floor(time % 60)
+  let minutes = Math.floor(time / 60) % 60
+  let hours = Math.floor(time / 3600)
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds
+  minutes = minutes < 10 ? `0${minutes}` : minutes
+  hours = hours < 10 ? `0${hours}` : hours
+
+  if (hours == 0) return `${minutes}:${seconds}`
+  
+  return `${hours}:${minutes}:${seconds}`
+}
+
+mainVideo.addEventListener('timeupdate', (e) => {
+  let {currentTime, duration} = e.target
+  let percent = (currentTime/duration)*100
+
+  progressBar.style.width = `${percent}%`
+  currentVideoTime.innerText = formatTime(currentTime)
+})
+
+//video timeline
+videoTimeline.addEventListener('click', (e) => {
+  let timelineWidth = videoTimeline.clientWidth
+
+  mainVideo.currentTime = (e.offsetX / timelineWidth) * mainVideo.duration
+})
+
+mainVideo.addEventListener('loadeddata', () => {
+  videoDuration.innerText = formatTime(mainVideo.duration)
+})
+
+const draggableProgressBar = (e => {
+  let timelineWidth = videoTimeline.clientWidth
+
+  progressBar.style.width = `${e.offsetX}px`
+  mainVideo.currentTime = (e.offsetX / timelineWidth) * mainVideo.duration
+  currentVideoTime.innerText = formatTime(mainVideo.currentTime)
+})
+
+videoTimeline.addEventListener('mousedown', () => {
+  videoTimeline.addEventListener('mousemove', draggableProgressBar)
+})
+
+videoTimeline.addEventListener('mouseup', () => {
+  videoTimeline.removeEventListener('mousemove', draggableProgressBar)
+})
+
+videoTimeline.addEventListener('mousemove', (e) => {
+  const progressTime = document.querySelector('.progress-area span')
+  let offsetX = e.offsetX
+  let timelineWidth = videoTimeline.clientWidth
+  let progressBarTime = (e.offsetX / timelineWidth) * mainVideo.duration
+
+  progressTime.style.left = `${offsetX}px`
+  progressTime.innerText = formatTime(progressBarTime)
+})
+
+//volume 
+volumeBtn.addEventListener('click', () => {
+  !volumeBtn.classList.contains('fa-volume-high') 
+  ? (mainVideo.volume = 0.5,
+   volumeBtn.classList.replace('fa-volume-xmark', 'fa-volume-high')) 
+  : (mainVideo.volume = 0.0,
+    volumeBtn.classList.replace('fa-volume-high', 'fa-volume-xmark'))
+
+  volumeSlider.value = mainVideo.volume
+})
+
+volumeSlider.addEventListener('input', (e) => {
+  mainVideo.volume = Number(e.target.value)
+
+  mainVideo.volume === 0 
+  ? volumeBtn.classList.replace('fa-volume-high', 'fa-volume-xmark') 
+  : volumeBtn.classList.replace('fa-volume-xmark', 'fa-volume-high')
+})
+
+//play pause 
+playPauseBtn.addEventListener('click', () => {
+  mainVideo.paused ? mainVideo.play() : mainVideo.pause()
+})
+
+mainVideo.addEventListener('play', () => {
+  playPauseBtn.classList.replace('fa-play', 'fa-pause')
+})
+
+mainVideo.addEventListener('pause', () => {
+  playPauseBtn.classList.replace('fa-pause', 'fa-play')
+})
+
+//skip 
+skipBackward.addEventListener('click', () => {
+  mainVideo.currentTime -= 5
+})
+
+skipForward.addEventListener('click', () => {
+  mainVideo.currentTime += 5
+})
+
+//speed options
+speedBtn.addEventListener('click', () => {
+  speedOptions.classList.toggle('show')
+})
+
+document.addEventListener('click', (e) => {
+  e.target.tagName !=='SPAN' || e.target.className !== 'material-symbols-outlined' ? speedOptions.classList.remove('show') : 'undefined'
+})
+
+speedOptionsItems.forEach((option) => {
+  option.addEventListener('click', () => {
+    mainVideo.playbackRate = option.dataset.speed;
+    speedOptions.querySelector('.active-option').classList.remove('active-option')
+    option.classList.add('active-option')
+  })
+})
+
+//picture in picture mode
+picInPicBtn.addEventListener('click', () => {
+  mainVideo.requestPictureInPicture()
+})
+
+//fullscreen mode
+fullscreenBtn.addEventListener('click', () => {
+  mainVideo.requestFullscreen()
+})
--- README.md
+++ README.md
@@ -9,32 +9,15 @@ This project showcases the creation of responsive web layouts using only HTML, C
 - **Responsive Design**: The layouts adjust seamlessly to different screen sizes and orientations.
 - **Pure HTML, CSS, and JavaScript**: No frameworks are used, demonstrating fundamental web development skills.
 - **Cross-Browser Compatibility**: Ensures a consistent experience across various browsers.
-- **Accessibility**: Incorporates best practices to make the web pages accessible to all users.
-
-## Project Structure
-
-
 
 ## How to Run
 
 1. Clone the repository:
    ```
-   git clone https://github.com/yourusername/responsive-layouts.git
+   git clone https://github.com/tsg1204/create-beautiful.git
    ```
 2. Navigate to the project directory:
    ```
-   cd responsive-layouts
+   cd create-beautiful
    ```
 3. Open `index.html` in your web browser to view the responsive layouts.
-
-## Customization
-
-Feel free to customize the layouts to suit your needs. You can modify the HTML, CSS, and JavaScript files to change the structure, style, and behavior of the web pages.
-
-## Contributing
-
-If you'd like to contribute to this project, please fork the repository and create a pull request with your changes. Contributions, suggestions, and improvements are welcome!
-
----
-
-Feel free to adjust any section as per your specific project details.
